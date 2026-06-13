b59859421d1a3803ee1afb5b00c3799dd0e7ad57
Yousefamr26 <2202291@student.eelu.edu.eg>
2026-06-13 21:58:36 +0300

    Delete DataAccess directory

--- DataAccess/IRepository/Analytics/IJobAnalyticsRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using DataAccess.Entities.Job;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface IJobAnalyticsRepository
-{
-    Task<Dictionary<string, int>> GetByTypeAndLevelAsync();
-}
--- DataAccess/IRepository/Analytics/IWorkshopAnalyticsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataAccess.Entities.Workshop;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface IWorkshopAnalyticsRepository
-{
-    Task<int> GetTotalParticipantsAsync();
-    Task<Dictionary<string, int>> GetByTypeAsync();
-}
