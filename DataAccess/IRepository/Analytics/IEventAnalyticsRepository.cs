a1ffe759e3bb6fcecea53ed570766a47a0ee67f9
Yousefamr26 <2202291@student.eelu.edu.eg>
2026-06-18 17:53:50 +0300

    Delete DataAccess directory

--- DataAccess/IRepository/Analytics/IEventAnalyticsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataAccess.Entities.Events;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-public interface IEventAnalyticsRepository
-{
-    Task<int> GetTotalParticipantsAsync();
-    Task<Dictionary<string, int>> GetByModeAsync();
-}
